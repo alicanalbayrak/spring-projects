c822278ff6d785faa1c735c3b2b8eaa09e66e144
Ali Can Albayrak <albayrakalican@gmail.com>
2017-03-06 17:27:56 +0300

    JSON, XML negotiation manager

--- sample-spring-mvc/src/main/java/com/gilmour/controller/MinutesController.java
+++ sample-spring-mvc/src/main/java/com/gilmour/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.gilmour.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gilmour.model.Activity;
 import com.gilmour.model.Exercise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MinutesController {
@@ -28,6 +34,26 @@ public class MinutesController {
 		System.out.println("exercising: " + exercise.getMinutes());
 
 		return "addMinutes";
+	}
+
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities() {
+
+		List<Activity> activities = new ArrayList<>();
+
+		Activity run = new Activity();
+		run.setDesc("run");
+		activities.add(run);
+
+		Activity bike = new Activity();
+		bike.setDesc("bike");
+		activities.add(bike);
+
+		Activity swim = new Activity();
+		swim.setDesc("swim");
+		activities.add(swim);
+
+		return activities;
 	}
 
 }
