433fb81136bffeb67ba1b5d0eec0186c48baf6e9
Ali Can Albayrak <albayrakalican@gmail.com>
2017-03-06 13:36:09 +0300

    Chaining: (forward, redirect)

--- sample-spring-mvc/src/main/java/com/gilmour/controller/MinutesController.java
+++ sample-spring-mvc/src/main/java/com/gilmour/controller/MinutesController.java
@@ -13,6 +13,19 @@ public class MinutesController {
 
 		System.out.println("exercise: " + exercise.getMinutes());
 
+		// ByPasses view resolver
+		return "forward:addMoreMinutes.html";
+
+		// Creates a new request
+		// return "redirect:addMoreMinutes.html";
+	}
+
+	@RequestMapping(value = "/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+
+		System.out.println("exercising: " + exercise.getMinutes());
+
 		return "addMinutes";
 	}
+
 }
