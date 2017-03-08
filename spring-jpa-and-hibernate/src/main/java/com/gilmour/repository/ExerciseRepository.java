ef73848d6b0a75d1a7f720bd3334b45860907eb1
Ali Can Albayrak <albayrakalican@gmail.com>
2017-03-08 14:46:26 +0300

    Spring Data JPA example

--- spring-jpa-and-hibernate/src/main/java/com/gilmour/repository/ExerciseRepository.java
+++ spring-jpa-and-hibernate/src/main/java/com/gilmour/repository/ExerciseRepository.java
@@ -1,9 +1,10 @@
 package com.gilmour.repository;
 
 import com.gilmour.model.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ExerciseRepository {
-
-	Exercise save(Exercise exercise);
+@Repository("exerciseRepository")
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
 }
