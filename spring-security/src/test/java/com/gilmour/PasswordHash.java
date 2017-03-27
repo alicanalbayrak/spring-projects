258e04926e51c775c05141bf63f6e87d95d30ac0
Ali Can Albayrak <albayrakalican@gmail.com>
2017-03-27 15:40:45 +0300

    bcrypt hash

--- spring-security/src/test/java/com/gilmour/PasswordHash.java
+++ spring-security/src/test/java/com/gilmour/PasswordHash.java
@@ -2,11 +2,19 @@ package com.gilmour;
 
 import junit.framework.TestCase;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by alicana on 27/03/2017.
  */
 public class PasswordHash extends TestCase {
+
+	public void testBcryptHash(){
+		String password = "wick";
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+		System.out.println(hashedPassword);
+	}
 
 	public void testMD5Hash() {
 		String password = "wick";
